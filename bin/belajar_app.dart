df605eb60476025d25c8653a1254b25357906d7b
sanji2207 <saepullohepul565@gmail.com>
2023-05-20 10:44:08 +0700

    menambahkan library

--- bin/belajar_app.dart
+++ bin/belajar_app.dart
@@ -1,5 +1,8 @@
 import 'package:belajar_app/belajar_app.dart' as belajar_app;
+import 'package:nama_library/nama_library.dart' as nama;
 
 void main(List<String> arguments) {
   print('Hello world: ${belajar_app.calculate()}!');
+
+  nama.sayNama('Toni');
 }
